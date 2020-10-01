3a36d7448f6c0c4d5aa702adf4a1152c731cd0fc
aosses <adrianoosses9@gmail.com>
2020-10-01 20:11:02 +0200

    version entregable 1. hacer readme

--- script/funciones.js
+++ script/funciones.js
@@ -6,3 +6,13 @@ function enciendeApaga(){
         document.getElementById('luzVerde').style.opacity=0;
     }
 }
+function bajaBotonI(){
+    /*document.getElementById('botonSuperiorI').style.top="7em"*/
+    //var opacidad=document.getElementById('luzVerde').style.opacity;
+    
+    if(document.getElementById('botonSuperiorI').style.top == "6.5em"){
+        document.getElementById('botonSuperiorI').style.top="7.2em";
+    }else{
+        document.getElementById('botonSuperiorI').style.top="6.5em";
+    }
+}
